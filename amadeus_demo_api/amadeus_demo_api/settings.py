5d515d6472929ba380d72621146f0f8e012008d5
averma8 <averma8@hotmail.com>
2023-03-30 08:05:27 +0530

    UpdatedAllowed

--- amadeus_demo_api/amadeus_demo_api/settings.py
+++ amadeus_demo_api/amadeus_demo_api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'q77h6c^4l2fkcbfwnusbaas#()#mp48hj@_93o#h_+62u%%2t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE', True)
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'web-production-9bf3.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'monkfish-app-e9g5q.ondigitalocean.app']
 
 # Application definition
 
